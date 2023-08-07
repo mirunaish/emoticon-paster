9d68c69ed6b4c4a1566aa646132d466874910112
miruna <miruna.palaghean@gmail.com>
2023-08-07 17:42:20 +0300

    v1

---
+++ program.py
@@ -0,0 +1,67 @@
+import json
+import pyperclip
+import tkinter as tk
+
+## load emoticons
+emoticons = ""
+with open("emoticons.json", 'r', encoding="utf8") as fp:
+  emoticons = json.load(fp)
+
+## create tkinter window
+window = tk.Tk()
+# make it stay on top
+window.attributes('-topmost', 'true')
+# set size
+window.geometry("350x350")
+# prevent resizing
+window.resizable(width=False, height=False)
+# define font
+bigfont = ("Arial", 14)
+
+## create frames
+tab_frame = tk.Frame(master=window)
+tab_frame.pack(side="top", fill="x", expand=False)
+
+emoticons_frame = tk.Frame(master=window)
+emoticons_frame.pack(side="top", fill="both", expand=True)
+
+# the text widget enables automatic "word wrapping" of buttons
+# https://stackoverflow.com/questions/69846517/how-to-auto-wrap-widget-in-tkinter
+text = tk.Text(master=emoticons_frame, wrap="word")
+text.configure(state="disabled", cursor='')
+text.pack(fill="both", expand=True)
+
+## load emoticons on selected tab
+def select_tab(tab):
+  # remove old emoticons
+  # https://stackoverflow.com/questions/15781802/python-tkinter-clearing-a-frame
+  for widget in text.winfo_children():
+    widget.destroy()
+
+  # add new emoticons
+  for i, emoticon in enumerate(emoticons[list(emoticons)[tab]]):
+    button = tk.Button(
+        text=emoticon,
+        font=bigfont,
+        bg="white",
+        fg="black",
+        # https://stackoverflow.com/a/47296482
+        command=lambda e=emoticon: pyperclip.copy(e),
+        master=text
+    )
+    text.window_create("end", window=button)
+  
+## add tab buttons
+for i, title in enumerate(emoticons.keys()):
+  tk.Button(
+      text=title,
+      bg="white",
+      fg="black",
+      command=lambda tab=i: select_tab(tab),
+      master=tab_frame
+  ).pack(side="left", fill="both", expand=True)
+
+## initial load
+select_tab(0)
+
+window.mainloop()
